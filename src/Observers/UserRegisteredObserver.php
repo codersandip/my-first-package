991f321649a6be9a8345aa8494bc51928fc32bc3
sandip Tawhare <sandip.tawhare@fyntune.com>
2022-03-07 00:08:09 +0530

    solved the problem

--- src/Observers/UserRegisteredObserver.php
+++ src/Observers/UserRegisteredObserver.php
@@ -10,54 +10,11 @@ class UserRegisteredObserver
     /**
      * Handle the User "created" event.
      *
-     * @param  \App\Models\User  $user
+    //  * @param  \App\Models\User  $user
      * @return void
      */
     public function created(UserCreatedMailableNotificationContracts $user)
     {
         $user->sendUserCreatedMailableNotification();
-    }
-
-    /**
-     * Handle the User "updated" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
-    public function updated(User $user)
-    {
-    }
-
-    /**
-     * Handle the User "deleted" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
-    public function deleted(User $user)
-    {
-        //
-    }
-
-    /**
-     * Handle the User "restored" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
-    public function restored(User $user)
-    {
-        //
-    }
-
-    /**
-     * Handle the User "force deleted" event.
-     *
-     * @param  \App\Models\User  $user
-     * @return void
-     */
-    public function forceDeleted(User $user)
-    {
-        //
     }
 }
